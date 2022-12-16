3c07e7e81e2a565f190997781a0a944081db91b8
Vishal Yadav <110458015+raovishalyadav@users.noreply.github.com>
2022-12-16 15:03:10 +0530

    Delete app directory

--- app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
-from .models import Apicrud
-from .serializers import serializers_Apicrud
-
-class cruddata(ModelViewSet):
-    queryset = Apicrud.objects.all()
-    serializer_class = serializers_Apicrud
-    
